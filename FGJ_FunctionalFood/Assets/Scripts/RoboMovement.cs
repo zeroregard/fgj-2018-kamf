86a2c08bcbeda9bf35783b867c1c157a7f93bcf6
Aleksi Skantz <aleksi.skantz@siili.com>
2018-01-27 11:47:33 +0200

    add first movementscript

---
+++ FGJ_FunctionalFood/Assets/Scripts/RoboMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboMovement : MonoBehaviour {
+
+	public Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		rigidBody.AddForce(10.0f, 0, 0);
+
+	}
+}
