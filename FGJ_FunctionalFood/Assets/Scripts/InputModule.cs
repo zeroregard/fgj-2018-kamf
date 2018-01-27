ab1e30243c17503fb5841a76bf190fa7bee0a097
Mathias Nørregaard <mathias.norregaard@futurice.com>
2018-01-27 12:32:02 +0200

    Input module WIP

---
+++ FGJ_FunctionalFood/Assets/Scripts/InputModule.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace MadLagBots
+{
+    public enum InputType
+    {
+        Left,
+        Right,
+        Forward,
+        Back,
+        Attack
+    }
+
+    public class InputModule : MonoBehaviour
+    {
+        [Range(1, 4)]
+        public int Player;
+        private const float _inputDelayMs = 200;
+        private bool _acceptInput = true;
+
+        void Update()
+        {
+            if (_acceptInput)
+            {
+                GetInput();
+            }
+        }
+
+        private void GetInput()
+        {
+            var p = $"Player{Player}";
+            var hoz = Input.GetAxis($"{p}_Horizontal");
+            var ver = Input.GetAxis($"{p}_Vertical");
+            var attack = Input.GetKey($"{p}_Attack");
+
+            if (hoz == 0 && ver == 0 && !attack)
+            {
+                return;
+            }
+            if (attack && hoz != 0 && ver != 0)
+            {
+                print("Honk!");
+            }
+            else if (attack)
+            {
+                SendInput(InputType.Attack);
+                _acceptInput = false;
+            }
+            else if (hoz != 0)
+            {
+                if (hoz > 0)
+                {
+                    SendInput(InputType.Right);
+                }
+                else
+                {
+                    SendInput(InputType.Left);
+                }
+            }
+            else if (ver != 0)
+            {
+                if (ver > 0)
+                {
+                    SendInput(InputType.Forward);
+                }
+                else
+                {
+                    SendInput(InputType.Back);
+                }
+            }
+
+            if (_acceptInput == false)
+            {
+                Observable.Timer(System.TimeSpan.FromMilliseconds(_inputDelayMs)).Subscribe(_ =>
+                {
+                    _acceptInput = true;
+                });
+            }
+        }
+
+        private void SendInput(InputType input)
+        {
+            // show stuff on screen
+            // send to robo module
+        }
+    }
+}
