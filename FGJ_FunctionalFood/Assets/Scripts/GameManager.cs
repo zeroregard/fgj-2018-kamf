08f1e2189214c9c1fa2d24ad46a7dd82c565d149
Mathias Nørregaard <mathias.norregaard@futurice.com>
2018-01-27 17:10:25 +0200

    Whups

---
+++ FGJ_FunctionalFood/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MadLagBots
+{
+    public class GameManager : MonoBehaviour
+    {
+        public RoboModule HammerBotPrefab;
+		public Transform SpawnPointOne;
+		public Transform SpawnPointTwo;
+        // Use this for initialization
+        void Start()
+        {
+			BeginGame();
+        }
+
+        void BeginGame()
+        {
+			var p1 = Instantiate(HammerBotPrefab, SpawnPointOne.position, Quaternion.identity);
+			p1.GetComponent<InputModule>().Player = 1;
+			var p2 = Instantiate(HammerBotPrefab, SpawnPointTwo.position, Quaternion.identity);
+			p2.GetComponent<InputModule>().Player = 2;
+        }
+
+		void PlayerDied(RoboModule player)
+		{
+			// stuff	
+		}
+
+		void GameOver()
+		{
+			// stuff
+		}
+		
+    }
+}
