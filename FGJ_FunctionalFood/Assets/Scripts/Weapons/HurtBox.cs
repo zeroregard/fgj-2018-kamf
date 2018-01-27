74dcd73cddc9a558ffb6a4ec8af21133f5f0986d
Mathias Nørregaard <mathias.norregaard@futurice.com>
2018-01-27 23:50:13 +0200

    Punch bot

--- FGJ_FunctionalFood/Assets/Scripts/Weapons/HurtBox.cs
+++ FGJ_FunctionalFood/Assets/Scripts/Weapons/HurtBox.cs
@@ -8,31 +8,38 @@ namespace MadLagBots
     public class HurtBox : MonoBehaviour
     {
         [SerializeField] private HealthModule _self;
-		public float Damage = 10;
-		private bool _isHurting = false;
-		private bool _canHurt = true;
-		private float _hurtCooldown = 0.85f;
+        public float Damage = 10;
+        private bool _isHurting = false;
+        private bool _canHurt = true;
+        private float _hurtCooldown = 0.85f;
+        public float PushForce = 1;
 
-		public void SetHurting(bool hurting)
-		{
-			_isHurting = hurting;
-		}
+        public void SetHurting(bool hurting)
+        {
+            _isHurting = hurting;
+        }
 
         void OnTriggerStay(Collider col)
         {
-			if(_isHurting && _canHurt)
-			{
-				var healthModule = col.gameObject.GetComponent<HealthModule>();
-				if(healthModule != null && healthModule != _self)
-				{
-					_canHurt = false;
-					Observable.Timer(TimeSpan.FromSeconds(_hurtCooldown)).Subscribe(_ =>
-					{
-						_canHurt = true;
-					});
-					healthModule.Hurt(Damage);
-				}
-			}
+            if (_isHurting && _canHurt)
+            {
+                var healthModule = col.gameObject.GetComponent<HealthModule>();
+                if (healthModule != null && healthModule != _self)
+                {
+                    _canHurt = false;
+                    Observable.Timer(TimeSpan.FromSeconds(_hurtCooldown)).Subscribe(_ =>
+                    {
+                        _canHurt = true;
+                    });
+                    healthModule.Hurt(Damage);
+                    var rb = col.gameObject.GetComponent<Rigidbody>();
+                    if (rb != null)
+                    {
+                        var localDir = transform.right;
+                        rb.AddForce(-transform.TransformDirection(localDir.x, localDir.y, localDir.z) * PushForce, ForceMode.Force);
+                    }
+                }
+            }
         }
     }
 }
---
+++ FGJ_FunctionalFood/Assets/Scripts/Weapons/WeaponPunch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UniRx;
+namespace MadLagBots
+{
+    public class WeaponPunch : WeaponBase
+    {
+        [SerializeField] private Transform _puncher;
+        [SerializeField] private Vector3 _punchActivated;
+        [SerializeField] private Vector3 _punchDeactivated;
+        [SerializeField] private HurtBox _hurtbox;
+
+        protected override void DoAttack()
+        {
+            var downTime = _cooldown / 5f;
+            var upTime = _cooldown - downTime;
+            _puncher.DOLocalMove(_punchActivated, downTime).SetEase(Ease.InBack).OnComplete(() =>
+            {
+                _hurtbox.SetHurting(true);
+                Observable.Timer(System.TimeSpan.FromSeconds(upTime / 2f)).Subscribe(_ =>
+                  {
+                      _hurtbox.SetHurting(false);
+                  });
+                _puncher.DOLocalMove(_punchDeactivated, upTime).SetEase(Ease.OutBack);
+            });
+        }
+    }
+}
