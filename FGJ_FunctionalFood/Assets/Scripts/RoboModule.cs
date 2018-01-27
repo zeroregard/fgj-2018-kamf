bd4b40c03a87eaa14a42746bdde74b794657b071
Aleksi Skantz <aleksi.skantz@siili.com>
2018-01-27 13:52:41 +0200

    scenes, derp

--- FGJ_FunctionalFood/Assets/Scripts/RoboModule.cs
+++ FGJ_FunctionalFood/Assets/Scripts/RoboModule.cs
@@ -7,7 +7,7 @@ namespace MadLagBots
 	public class RoboModule : MonoBehaviour {
 
 		public float thrust;
-		public float torque = 5;
+		public float torque;
 		public Rigidbody rb;
 
 		public void HandleInput(InputType input) 
