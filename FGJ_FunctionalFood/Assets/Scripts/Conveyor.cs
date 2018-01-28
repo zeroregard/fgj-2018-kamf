bcb3c335ec9fb4c1e14f1c8cc8ba5297b9876f19
Mathias Nørregaard <mathias.norregaard@futurice.com>
2018-01-28 03:19:12 +0200

    Real sweet conveyor belt

---
+++ FGJ_FunctionalFood/Assets/Scripts/Conveyor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public float ConveyorSpeed;
+    public MeshRenderer ConveyorMesh;
+
+    private HashSet<Rigidbody> _currentColliders = new HashSet<Rigidbody>();
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        ConveyorMesh.material.SetTextureOffset("_MainTex", new Vector2(0, ConveyorSpeed * Time.time));
+        foreach (var v in _currentColliders)
+        {
+            if (v.velocity.magnitude < 1.5f)
+            {
+                v.AddForce(transform.TransformDirection(transform.forward) * ConveyorSpeed * 3.6f);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        var rb = col.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            _currentColliders.Add(rb);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        var rb = col.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            _currentColliders.Remove(rb);
+        }
+    }
+}
