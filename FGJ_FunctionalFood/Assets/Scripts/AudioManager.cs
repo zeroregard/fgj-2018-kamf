41f7e54d9ba3b80da785a716997df13128791638
Konstantin Zudov <co@zudov.me>
2018-01-28 04:43:30 +0200

    Make death announcements less loud

--- FGJ_FunctionalFood/Assets/Scripts/AudioManager.cs
+++ FGJ_FunctionalFood/Assets/Scripts/AudioManager.cs
@@ -15,13 +15,20 @@ public class AudioManager : MonoBehaviour
     public AudioSource announcer;
 
     void Start()
-    {
+    {   
+        AudioPlayerOne.volume = 1.0f;
+        AudioPlayerTwo.volume = 1.0f;
+        announcer.volume = 0.7f;
+        PlayMusic();
+    }
+    
+    void PlayMusic() {
         AudioPlayerOne.clip = IntroSong;
         AudioPlayerOne.loop = false;
         AudioPlayerOne.Play();
-		AudioPlayerTwo.clip = ThemeSong;
-		AudioPlayerTwo.loop = true;
-		AudioPlayerTwo.PlayScheduled(AudioSettings.dspTime + IntroSong.length);
+        AudioPlayerTwo.clip = ThemeSong;
+        AudioPlayerTwo.loop = true;
+        AudioPlayerTwo.PlayScheduled(AudioSettings.dspTime + IntroSong.length);    
     }
     
     private AudioClip randomKnockout()
