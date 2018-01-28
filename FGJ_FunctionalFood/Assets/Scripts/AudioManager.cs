79c923183ab8eb573516d3d0b04e86844ae83621
Mathias Nørregaard <mathias.norregaard@futurice.com>
2018-01-28 12:04:29 +0200

    Fixed music stopping2 2

--- FGJ_FunctionalFood/Assets/Scripts/AudioManager.cs
+++ FGJ_FunctionalFood/Assets/Scripts/AudioManager.cs
@@ -8,17 +8,19 @@ public class AudioManager : MonoBehaviour
     public AudioClip IntroSong;
     public AudioClip ThemeSong;
     public AudioSource AudioPlayerOne;
-	public AudioSource AudioPlayerTwo;
-    
+    public AudioSource AudioPlayerTwo;
+
     public List<AudioClip> knockouts;
-    
+
     public AudioSource announcer;
-    
+
     // I couldn't find any List.shuffle method around
     // Sombedy, please get rid of that madness
-    private void ShuffleKnockouts() {
+    private void ShuffleKnockouts()
+    {
         int n = knockouts.Count;
-        while (n > 1) {
+        while (n > 1)
+        {
             int k = (Random.Range(0, n));
             n--;
             AudioClip value = knockouts[k];
@@ -28,29 +30,30 @@ public class AudioManager : MonoBehaviour
     }
 
     void Start()
-    {   
+    {
         AudioPlayerOne.volume = 1.0f;
         AudioPlayerTwo.volume = 1.0f;
         announcer.volume = 0.7f;
         PlayMusic();
         ShuffleKnockouts();
     }
-    
-    void PlayMusic() {
+
+    void PlayMusic()
+    {
         AudioPlayerOne.clip = IntroSong;
         AudioPlayerOne.loop = false;
         AudioPlayerOne.Play();
         AudioPlayerTwo.clip = ThemeSong;
         AudioPlayerTwo.loop = true;
-        AudioPlayerTwo.PlayScheduled(AudioSettings.dspTime + IntroSong.length);    
+        AudioPlayerTwo.PlayScheduled(AudioSettings.dspTime + IntroSong.length);
     }
-    
+
     public void AnnounceDeath(int playerId)
     {
         print("YOU'VE GOT KNOCKED OUT");
         announcer.clip = knockouts[playerId];
         announcer.loop = false;
         announcer.Play();
-  
+
     }
 }
